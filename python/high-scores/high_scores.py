ca87e2367c96f3388068b4ba883b7758702eeb18
Jean-Loup Adde <jean-loup.adde@juanwolf.fr>
2019-04-15 08:54:47 +0100

    :sparkles:(python): Add high score exercise

---
+++ python/high-scores/high_scores.py
@@ -0,0 +1,16 @@
+class HighScores(object):
+    def __init__(self, scores):
+        self.scores = scores
+
+    def personal_top_three(self):
+        return self._get_best_scores(3)
+
+    def latest(self):
+        return self.scores[len(self.scores) - 1]
+
+    def personal_best(self):
+        return self._get_best_scores(1)[0]
+
+    def _get_best_scores(self, n):
+        """_get_best_scores returns the n best scores from this instance of HighScores"""
+        return sorted(self.scores, reverse=True)[0:n]
