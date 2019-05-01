6dbaebb14cd5cfc5708fb07add9c003b254ac7ce
Jean-Loup Adde <jean-loup.adde@juanwolf.fr>
2019-05-01 22:47:08 +0100

    :recycle:(rust): Better reverse string handling

--- rust/reverse-string/src/lib.rs
+++ rust/reverse-string/src/lib.rs
@@ -3,8 +3,9 @@ extern crate unicode_segmentation;
 use unicode_segmentation::UnicodeSegmentation;
 
 pub fn reverse(input: &str) -> String {
-    let mut g: Vec<&str> = UnicodeSegmentation::graphemes(input, true).collect::<Vec<&str>>();
-    g.reverse();
+    let g = UnicodeSegmentation::graphemes(input, true)
+        .rev()
+        .collect::<Vec<&str>>();
 
     return g.join("");
 }
