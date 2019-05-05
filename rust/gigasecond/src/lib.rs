7354a610d82659983ba7df6ed1f8f00b6c5628d3
Jean-Loup Adde <jean-loup.adde@juanwolf.fr>
2019-05-05 11:42:07 +0100

    :recycle:(rust): Refactor gigasecond
    extern crate is not needed anymore in rust 2018.
    As well Duration is already part of the chrono crate and
    I removed a magic number from the after function

--- rust/gigasecond/src/lib.rs
+++ rust/gigasecond/src/lib.rs
@@ -1,9 +1,8 @@
-extern crate chrono;
+use chrono::{DateTime, Duration, Utc};
 
-use chrono::{DateTime, Utc};
-use time::Duration;
+const GIGA: i64 = 1_000_000_000;
 
 // Returns a Utc DateTime one billion seconds after start.
 pub fn after(start: DateTime<Utc>) -> DateTime<Utc> {
-    start + Duration::seconds(1000000000)
+    start + Duration::seconds(GIGA)
 }
