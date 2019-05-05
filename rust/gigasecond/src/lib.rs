0d2d8db6dbaa3bd92c287be1f9e5883f2390df09
Jean-Loup Adde <jean-loup.adde@juanwolf.fr>
2019-05-05 11:39:30 +0100

    :sparkles:(rust): Add gigasecond exercise

---
+++ rust/gigasecond/src/lib.rs
@@ -0,0 +1,9 @@
+extern crate chrono;
+
+use chrono::{DateTime, Utc};
+use time::Duration;
+
+// Returns a Utc DateTime one billion seconds after start.
+pub fn after(start: DateTime<Utc>) -> DateTime<Utc> {
+    start + Duration::seconds(1000000000)
+}
