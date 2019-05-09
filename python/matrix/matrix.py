395d026ac1910f41de3b733211cef48edba0ac6c
Jean-Loup Adde <jean-loup.adde@juanwolf.fr>
2019-05-09 22:32:00 +0100

    :recycle:(python/matrix): Change list initialisation to one liners

--- python/matrix/matrix.py
+++ python/matrix/matrix.py
@@ -1,20 +1,12 @@
 class Matrix(object):
     def __init__(self, matrix_string):
-        self.rows = []
-
-        for row in matrix_string.splitlines():
-            formatted_column = [
-                int(column_item)
-                for column_item in row.split()
-            ]
-            self.rows.append(formatted_column)
+        self.rows = [
+            [int(column_item) for column_item in row.split()]
+            for row in matrix_string.splitlines()
+        ]
 
     def row(self, index):
         return self.rows[index - 1]
 
     def column(self, index):
-        column = []
-        for row in self.rows:
-            column.append(row[index - 1])
-
-        return column
+        return  [row[index - 1] for row in self.rows]
