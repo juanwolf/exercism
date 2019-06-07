2595a5442923b90c6ca28e8ade7d60bca437c3a5
Jean-Loup Adde <jean-loup.adde@juanwolf.fr>
2019-06-07 18:29:24 +0100

    :sparkles:(go/proverb): Add solution

--- go/proverb/proverb.go
+++ go/proverb/proverb.go
@@ -1,15 +1,23 @@
-// This is a "stub" file.  It's a little start on your solution.
-// It's not a complete solution though; you have to write some code.
-
-// Package proverb should have a package comment that summarizes what it's about.
-// https://golang.org/doc/effective_go.html#commentary
 package proverb
 
-// Proverb should have a comment documenting it.
-func Proverb(rhyme []string) []string {
-	// Write some code here to pass the test suite.
-	// Then remove all the stock comments.
-	// They're here to help you get started but they only clutter a finished solution.
-	// If you leave them in, reviewers may protest!
-	return []string{}
+import (
+	"fmt"
+)
+
+const (
+	forWantTemplate string = "For want of a %s the %s was lost."
+	lastVerse       string = "And all for the want of a %s."
+)
+
+// Proverb returns the For Want of a Nail proverb
+func Proverb(rhymes []string) []string {
+	var proverb []string
+	for i, rhyme := range rhymes {
+		if i == len(rhymes)-1 {
+			proverb = append(proverb, fmt.Sprintf(lastVerse, rhymes[0]))
+		} else {
+			proverb = append(proverb, fmt.Sprintf(forWantTemplate, rhyme, rhymes[i+1]))
+		}
+	}
+	return proverb
 }
