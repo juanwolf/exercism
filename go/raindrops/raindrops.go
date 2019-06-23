4e7985ac718639fc0c5fb0234963a6e45cb74810
Jean-Loup Adde <jean-loup.adde@juanwolf.fr>
2019-06-23 23:16:28 +0100

    :memo:(go/raindrops): Add doc

--- go/raindrops/raindrops.go
+++ go/raindrops/raindrops.go
@@ -1,9 +1,18 @@
+// Package raindrops converts a number to a raindrop.
 package raindrops
 
 import (
 	"fmt"
 )
 
+/*
+Convert returns a string according to the number in parameter.
+It adds
+ 'Pling' if the number has 3 as a factor
+ 'Plang' if the number has 5 as a factor
+ 'Plong' if the number has 7 as a factor
+or returns the number's digit if none of the above.
+*/
 func Convert(number int) string {
 	result := ""
 	if number%3 == 0 {
