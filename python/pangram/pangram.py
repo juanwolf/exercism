a054c504dc2fffcbd94737fc6206877ed9f9fa84
Jean-Loup Adde <jean-loup.adde@thetrainline.com>
2020-06-05 16:59:00 +0100

    :recycle:(python): Refactor pangram solution

--- python/pangram/pangram.py
+++ python/pangram/pangram.py
@@ -1,15 +1,10 @@
-ALPHABET = "abcdefghijklmnopqrstuvwxyz"
+import string
 
 
 def is_pangram(sentence):
-    letter_found = {letter: False for letter in ALPHABET}
     sentence = sentence.lower()
-    for letter in sentence:
-        if letter in letter_found:
-            letter_found[letter] = True
-
-    for found in letter_found.values():
-        if not found:
-            return found
+    for letter in string.ascii_lowercase:
+        if letter not in sentence:
+            return False
 
     return True
