0a6070a35f036e73d02feebf5777003db66ccf34
Jean-Loup Adde <jean-loup.adde@juanwolf.fr>
2024-09-06 11:29:18 +0100

    :sparkles:(go): add strain implementation

--- go/strain/strain.go
+++ go/strain/strain.go
@@ -5,3 +5,26 @@ package strain
 // You will need typed parameters (aka "Generics") to solve this exercise.
 // They are not part of the Exercism syllabus yet but you can learn about
 // them here: https://go.dev/tour/generics/1
+//
+
+type predicate[T any] func(T) bool
+
+func Discard[T any](in []T, fn predicate[T]) []T {
+	res := []T{}
+	for _, v := range in {
+		if !fn(v) {
+			res = append(res, v)
+		}
+	}
+	return res
+}
+
+func Keep[T any](in []T, fn predicate[T]) []T {
+	res := []T{}
+	for _, v := range in {
+		if fn(v) {
+			res = append(res, v)
+		}
+	}
+	return res
+}
